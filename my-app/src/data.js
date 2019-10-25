fefd418bfe58476130ddfa5b98228c8f5e9febe0
Manon Ragnotti <manon@MacBook-Pro-de-Manon.local>
2019-10-25 15:21:46 +0200

    Add Firebase config for our database

---
+++ my-app/src/data.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaCRpLZw14WHI98FJw1SA0KBTxIY4MViQ",
+  authDomain: "studentslist.firebaseapp.com",
+  databaseURL: "https://studentslist.firebaseio.com",
+  projectId: "studentslist",
+  storageBucket: "studentslist.appspot.com",
+  messagingSenderId: "326573645561",
+  appId: "1:326573645561:web:2229e2f2fdda5feb57cc28"
+};
+
+firebase.initializeApp(firebaseConfig)
