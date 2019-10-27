864cd54d45aed42581f5804880663f9040f52231
Eugénie Roques <eugenieroques@Eugenies-MacBook-Air.local>
2019-10-27 18:16:22 +0100

    added server

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import firebaseConfig from './data.js';
 import './App.css';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDnJuNxfAMxxqrL7jw6Z4qh1WTAzS24eG0",
+  authDomain: "react-challenge-9e33c.firebaseapp.com",
+  databaseURL: "https://react-challenge-9e33c.firebaseio.com",
+  projectId: "react-challenge-9e33c",
+  storageBucket: "react-challenge-9e33c.appspot.com",
+  messagingSenderId: "494251381045",
+  appId: "1:494251381045:web:0fd143ba0a0f371c373c7c"
+};
+
 function App() {
   return (
     <div className="App">
