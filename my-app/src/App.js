e99b5c1ece9adf4c379804efc997656aafbb3204
Manon Ragnotti <manon@MacBook-Pro-de-Manon.local>
2019-11-14 10:32:56 +0100

    Update nav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import StudentList from './components/StudentList/StudentList';
-import Navigation from './components/Navigation/Navigation';
 import AddStudentComponent from './components/AddStudent/AddStudentComponent';
 import RegisterComponent from './components/Register/RegisterComponent';
 
@@ -18,18 +17,15 @@ function App() {
         <Navbar bg="dark" variant="dark">
           <Nav className="mr-auto">
           <Navbar.Brand href="/">HETIC</Navbar.Brand>
+            <Link className="navbar-dark navbar-nav nav-link navbar-expand" to="/students">Etudiants</Link>
             <Link to="/addStudent" className="navbar-dark navbar-nav nav-link navbar-expand">Ajouter un étudiant</Link>
-            <Link className="navbar-dark navbar-nav nav-link navbar-expand" to="/students">Students</Link>
-            <Link className="navbar-dark navbar-nav nav-link navbar-expand" to="/navigation">Nav</Link>
-            <Link className="navbar-dark navbar-nav nav-link navbar-expand" to="/Register">Register</Link>
-
+          <Link className="navbar-dark navbar-nav nav-link navbar-expand connexion" to="/Register">Connexion</Link>
           </Nav>
         </Navbar>
         <Switch>
           <Route  path="/register" component={RegisterComponent}  />
           <Route  path="/addStudent" component={AddStudentComponent}  />
           <Route  path="/students" component={StudentList}  />
-          <Route  path="/navigation" component={Navigation}  />
         </Switch>
       </Router>
     </div>
